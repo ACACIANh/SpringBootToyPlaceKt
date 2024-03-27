bcd1cff01f76b9c96fb53801de4554d2af9c38d7
ACACIAN <gkehdrn95@gmail.com>
2024-03-27 11:05:17 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/springboottoyplacekt/SpringBootToyPlaceKtApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springboottoyplacekt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootToyPlaceKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootToyPlaceKtApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springboottoyplacekt/SpringBootToyPlaceKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springboottoyplacekt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootToyPlaceKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
